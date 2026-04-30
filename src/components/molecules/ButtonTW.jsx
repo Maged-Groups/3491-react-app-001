b2e66eed6232b13de8e079c6b6632369b234ad6c
Maged Groups <magedyaseengroups@gmail.com>
2026-04-30 19:03:00 +0300

    fix: ButtonTW styles

--- src/components/molecules/ButtonTW.jsx
+++ src/components/molecules/ButtonTW.jsx
@@ -22,37 +22,19 @@ export default function ButtonTW({ shadow = 'shadow-none', className = '', onCli
 
     // Variants = primary|secondary|success|danger|warning|info
     const variants = {
-        secondary: {
-            background: '#cebcd5',
-            color: 'rgb(96, 22, 111)'
-        },
-        success: {
-            background: '#694',
-            color: '#282'
-        },
-        danger: {
-            background: '#e64',
-            color: '#622'
-        },
-        warning: {
-            background: '#dd2',
-            color: '#330'
-        },
-        info: {
-            background: '#79e',
-            color: '#23e'
-        },
-        primary: {
-            background: '#38cfc8',
-            color: '#08534f'
-        }
+        primary: 'text-violet-100 bg-violet-700',
+        secondary: 'text-gray-100 bg-gray-700',
+        success: 'text-green-100 bg-green-700',
+        danger: 'text-red-100 bg-red-700',
+        warning: 'text-yellow-100 bg-yellow-700',
+        info: 'text-sky-100 bg-sky-700',
     }
 
     // Opacity
     const opacity = disabled || isLoading ? 'opacity-30' : 'opacity-100';
 
     // Main Button Styles
-    const tWClasses = `inline-flex items-center justify-center gap-1 ${opacity} ${flexDirection} ${cursor} ${sizes[size]} ${hover} ${shadow} ${className}`;
+    const tWClasses = `inline-flex items-center justify-center gap-1 ${opacity} ${flexDirection} ${cursor} ${sizes[size]} ${hover} ${shadow} ${className} ${variants[variant]}`;
 
     return (
         <button onClick={onClick} className={tWClasses} disabled={disabled}>
