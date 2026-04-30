73522df42bf5940aeb0049417513e14d8591b71d
Maged Groups <magedyaseengroups@gmail.com>
2026-04-30 19:03:20 +0300

    feat: add cart to Navbar

--- src/components/organisms/Navbar.jsx
+++ src/components/organisms/Navbar.jsx
@@ -3,10 +3,11 @@ import { Link } from 'react-router'
 import Button from "../molecules/Button"
 import Logo from "../atoms/Logo"
 import Dropdown from "../molecules/Dropdown"
+import { FaCartShopping } from 'react-icons/fa6'
 
-export default function Navbar() {
+export default function Navbar({count}) {
     return (
-        <nav className="flex justify-between p-3">
+        <nav className="flex justify-between p-4 sticky top-0 bg-white shadow">
             <div>
                 <Logo />
             </div>
@@ -19,6 +20,10 @@ export default function Navbar() {
             </div>
 
             <div className="flex gap-3">
+                <div className='flex items-start gap-1 relative'>
+                    <FaCartShopping size='30' className='text-sky-600' />
+                    <span className='text-sm bg-sky-700 text-white w-6 h-6 rounded-full flex items-center justify-center absolute -top-5/12 left-5/12'>0</span>
+                </div>
                 <Button text='Sign up' iconName='ABC' />
                 <Button text='Sign in' iconName='LuLogIn' />
             </div>
